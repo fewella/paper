0a96a3917b1efc87e8935fcb01b2a937f198a0e7
Eunice Zhou <eunicexzhou@gmail.com>
2021-05-11 15:00:05 -0500

    extend data visuazliation in display_graph

--- python/Platform.py
+++ python/Platform.py
@@ -52,21 +52,58 @@ class Platform:
         self.core = c
         self.brain = b
     
-    def display_graph(self, symbol):
+    def display_graph(self, symbol, text="", debug=False):
         rsi_to_display = Core.dynamic_rsi[symbol][-10:]
         price_to_display = Core.historic_price[symbol][-10:]
+        if debug:
+            rsi_to_display = [
+                58.2946208838363, 
+                51.977050688175254, 
+                49.744122759386066, 
+                48.619445884434384, 
+                47.690495551751134, 
+                42.06453569149961, 
+                42.06453569149961, 
+                45.50560853393237, 
+                46.080425088089314, 
+                50.052131539962
+            ]
+            price_to_display = [400, 410, 420, 415, 417, 430, 405, 402, 421, 433]
+
         timestamps = []
+        ticks = []
+        now = datetime.datetime.now()
         for i in range(10):
-            curr = datetime.datetime.now() - datetime.timedelta(seconds=i)
-            timestamps.append(datetime.datetime.now().strftime("%H:%M"))
+            curr = now - datetime.timedelta(seconds=i)
+            timestamps.append(curr.strftime("%H:%M:%S"))
+            ticks.append(i)
+        timestamps = timestamps[::-1]
         
         fig, ax = plt.subplots()
-        ax.plot(timestamps, rsi_to_display, label="RSI")
-        ax.plot(timestamps, price_to_display, label="Price")
-        ax.legend(loc = 'upper left')
+        plt.xticks(ticks, timestamps, rotation=45)
+        
+        color1 = "tab:red"
+        color2 = "tab:blue"
+        
+        plot1 = ax.plot(rsi_to_display, label="RSI", color=color1)
+        ax.set_ylabel("RSI", color=color1)
+        ax.tick_params(axis='y', labelcolor=color1)
+        
+        ax2 = ax.twinx()
+        plot2 = ax2.plot(price_to_display, label="Price", color=color2)
+        ax2.set_ylabel('Price', color=color2)
+        ax2.tick_params(axis='y', labelcolor=color2)
+        
+        plots = plot1 + plot2
+        labels = [plot.get_label() for plot in plots]
+        plt.legend(plots, labels, loc='upper right', framealpha=0.7)
+        plt.annotate(text, (7, price_to_display[7]), xytext=(7.2, price_to_display[7] + 1))
+        plt.scatter(7, price_to_display[7], color='black', zorder=10)
+        plt.title("Stock: ", symbol)
 
         plt.show()
 
+        
     def should_buy(self, symbol):
         rsi_line = Core.dynamic_rsi[symbol]
 
@@ -82,7 +119,7 @@ class Platform:
                 # probabily increasing on this interval, so we assume not a good buy
                 return (False, [])
         if rsi_line[-2] > rsi_line[-3] and rsi_line[-1] > rsi_line[-2]:
-            self.display_graph(symbol)
+            self.display_graph(symbol, text="Buy Point")
             return (True, rsi_line[-10:])
         
         return (False, [])
@@ -101,7 +138,7 @@ class Platform:
             if curr < prev + epsilon:
                 return (False, [])
         if rsi_line[-2] < rsi_line[-3] and rsi_line[-1] < rsi_line[-2]:
-            self.display_graph(symbol)
+            self.display_graph(symbol, text="Sell Point")
             return (True, rsi_line[-10:])
 
         return (False, [])
