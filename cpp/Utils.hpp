60c1382f65f79431b694f63e9c8027ea2d8cf917
Eunice Zhou <eunicexzhou@gmail.com>
2021-05-12 19:09:59 -0500

    remove cpp folder

--- cpp/Utils.hpp
+++
@@ -1,9 +0,0 @@
-#include <nlohmann/json.hpp>
-using json = nlohmann::json;
-
-class Utils {
-    public:
-        // TODO: make custom parameters
-        static std::string get_orders_json();
-        static std::string post_order_json(std::string symbol, size_t n);
-};
